7d5eea77c0960297756d44806ec1b466810d3ada
Shivam Kapila <shivamkapila4@gmail.com>
2018-12-18 14:08:24 +0000

    NEW and CREATE routes added

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function (err, blogs) {
         if(err){
@@ -32,9 +33,26 @@ app.get("/blogs", function(req, res){
         } else {
     res.render("index", {blogs: blogs});            
         }
-    })
+    });
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function (req, res) {
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+    //then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function () {
     console.log("SERVER IS RUNNING!");
